8dee60f04c4cfe474725139e523c4412ba99bb1c
Alexis-With-logic <119010724+Alexis-With-logic@users.noreply.github.com>
2022-11-24 21:12:26 -0600

    Create _antilink2.js

---
+++ plugins/_antilink2.js
@@ -0,0 +1,24 @@
+let linkRegex = /https:/i 
+ export async function before(m, { isAdmin, isBotAdmin, text }) { 
+ if (m.isBaileys && m.fromMe) 
+ return !0 
+ if (!m.isGroup) return !1 
+ let chat = global.db.data.chats[m.chat] 
+ let bot = global.db.data.settings[this.user.jid] || {} 
+ const isGroupLink = linkRegex.exec(m.text) 
+ if (chat.antiLink2 && isGroupLink && !isAdmin) { 
+ if (isBotAdmin) { 
+ const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}` 
+ const linkThisGroup2 = `https://www.youtube.com/` 
+ const linkThisGroup3 = `https://youtu.be/` 
+ if (m.text.includes(linkThisGroup)) return !0 
+ if (m.text.includes(linkThisGroup2)) return !0 
+ if (m.text.includes(linkThisGroup3)) return !0 
+ }     
+ await conn.sendButton(m.chat, `*「 𝐀𝐍𝐓𝐈 𝐋𝐈𝐍𝐊𝐒 」*\n*𝙷𝙰𝚂𝚃𝙰 𝙻𝙰 𝚅𝙸𝚂𝚃𝙰 𝙱𝙰𝙱𝚈 👋, ${await this.getName(m.sender)} 𝚁𝙾𝙼𝙿𝙸𝚂𝚃𝙴𝚂 𝙻𝙰𝚂 𝚁𝙴𝙶𝙻𝙰𝚂 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙴𝚁𝙰𝚂 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙳𝙾...!!*${isBotAdmin ? '' : '\n\n*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*'}`, author, ['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺𝚂', '/disable antilink'], m)     
+ if (isBotAdmin && bot.restrict) { 
+ await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove') 
+ } else if (!bot.restrict) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚃𝙸𝙴𝙽𝙴 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙰𝙳𝙾 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙴*') 
+ } 
+ return !0 
+ }
